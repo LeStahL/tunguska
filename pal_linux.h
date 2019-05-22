c0800c52be8aad025552a58a7dd421d469425b99
Christoffer Anselm <c.anselm@paindevs.com>
2019-05-22 04:44:42 +0200

    * Moved all windows-specific code into pal_win32.h and added pal_linux.h with some stubs * Moved all shader includes to common.h and prefixed all uniform variable name defines, so they can later be moved into the respective headers without causing trouble

---
+++ pal_linux.h
@@ -0,0 +1,19 @@
+#ifndef PAL_LINUX_H
+#define PAL_LINUX_H
+
+#include <SDL2/SDL.h>
+
+void ExitProcess(int exit_code) {
+	exit(exit_code);
+}
+
+int main() {
+	//TODO(ca) Implement me
+	return 0;
+}
+
+void flip_buffers() {
+	//TODO(ca) Implement me
+}
+
+#endif
